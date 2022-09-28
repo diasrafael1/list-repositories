dd65c35b1d4995f89cce54a11bc35e18a983b6a1
Rafael Dias <rafaelldiias100@gmail.com>
2022-09-28 13:03:27 -0300

    Criada página 404.

--- src/pages/Error404/index.tsx
+++ src/pages/Error404/index.tsx
@@ -1,3 +1,13 @@
+import { Container } from "./styles";
+import { BiError } from "react-icons/bi";
+
 export default function Error() {
-  return <div></div>;
+  return (
+    <Container>
+      <h1>Error404</h1>
+      <BiError size={100} />
+
+      <h3>Página não encontrada! :(</h3>
+    </Container>
+  );
 }
---
+++ src/pages/Error404/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 80px;
+  }
+
+  h3 {
+    font-size: 30px;
+  }
+`;
